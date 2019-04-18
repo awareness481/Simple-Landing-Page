f1b1b22987aee96a009cf21f4a7e04ac97c5200f
Jesse Jafa <jesse.jafa@gmail.com>
2019-04-18 09:35:22 +0300

    Initial working Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: { main: './main.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  target: 'node', 
+  externals: [nodeExternals()],
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+          
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
